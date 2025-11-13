a69feceffd2af75e731c25c377e5857fada8cb39
Hiếu <thehieu090107@gmail.com>
2025-11-13 08:51:59 +0700

    Add files via upload

--- S17_2.cpp
+++ S17_2.cpp
@@ -2,39 +2,43 @@
 #include<string.h>
 
 int main(){
-	char str[100];
+	char s[100];
 	int choice;
 	while(1){
 		printf("\n+-------------------MENU-------------------+\n");
 		printf("| 1. Toan bo ky tu chuyen thanh chu hoa    |\n");
 		printf("| 2. Toan bo ky tu chuyen thanh chu thuong |\n");
 		printf("+------------------------------------------+\n");
-		
+
 		printf("Nhap mot chuoi: ");
-		fgets(str,100,stdin);
-	
+		fgets(s,100,stdin);
+
 		printf("\nNhap lua chon: ");
 		scanf("%d",&choice);
-		
+
 		switch(choice){
-			case 1: {
-			    for (int i = 0; i < strlen(str); i++) {
-                            str[i] = str[i] - 32;  
-                        
-                }        
-                    
-                 printf("Chuoi sau khi IN HOA: %s", str);
-			    break;}
-			case 2:{
-				for (int i = 0; i < strlen(str); i++) {
-                            str[i] = str[i] + 32;  
-                        
-                }    
-                 printf("Chuoi sau khi IN HOA: %s", str);
-			    break;    
+			case 1:
+				for (int i = 0; s[i] != '\0'; i++) {
+                if (s[i] >= 'a' && s[i] <= 'z') {
+                    s[i] = s[i] - 32;
+                }
+            }
+            printf("Chuoi in hoa: %s\n", s);
+            break;
+			case 2:
+				for (int i = 0; s[i] != '\0'; i++) {
+                if (s[i] >= 'A' && s[i] <= 'Z') {
+                    s[i] = s[i] + 32;
+                }
+            }
+            	printf("Chuoi in thuong: %s\n", s);
+            	break;
+
+            default:
+            	printf("lua chon khong hop le!\n");
+            	break;
+
 		}
-	}
-	}
+	}		
 	return 0;
 }
-
